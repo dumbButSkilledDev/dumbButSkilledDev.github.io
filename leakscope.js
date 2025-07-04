8c6cb28a6746986c53fd47340c84e1c80bc7678e
dumbButSkilledDev <111146406+dumbButSkilledDev@users.noreply.github.com>
2025-07-04 12:56:49 -0700

    Add files via upload

--- leakscope.js
+++ leakscope.js
@@ -9,6 +9,13 @@ function leakScope() {
         Leaker.prototype.__proto__ = new Proxy({}, {
             get(target, propertyName, receiver) {
                 log("[proxy trap] target: " + target + ", property: " + propertyName);
+
+                // Add Symbol.toPrimitive to receiver so it doesn't throw
+                receiver[Symbol.toPrimitive] = function(hint) {
+                    log("coercing receiver to primitive with hint: " + hint);
+                    return "dummy"; // Just return a harmless string
+                };
+
                 return receiver;
             }
         });
